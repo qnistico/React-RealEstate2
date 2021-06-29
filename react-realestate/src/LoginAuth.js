57ed83226143948f21daba391e178c9015500c33
Quinton Nistico <qnistico@aol.com>
2021-06-29 02:45:40 -0400

    form refs not working, attempting bootstrap install

--- react-realestate/src/LoginAuth.js
+++ react-realestate/src/LoginAuth.js
@@ -1,13 +1,52 @@
-import React from 'react'
+import React from "react";
 
 export default function LoginAuth() {
-    return (
-        <div className="login_auth">
-            <div className="login_auth_flex">
-                <div className="la_body">
-                    <h2>Login</h2>
-                </div>
+  return (
+    <div className="login_signup_auth">
+      <div className="login_auth_flex">
+        <div className="signup_body">
+          <h2>Sign Up</h2>
+          <form id="login_form" className="contact_form">
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                className="form-control form_input"
+                aria-describedby="emailHelp"
+                placeholder="Email"
+                ref={emailRef}
+                required
+              />
             </div>
+            <div className="form-group">
+              <label htmlFor="Password">Password</label>
+              <input
+                type="password"
+                className="form-control form_input"
+                aria-describedby="passwordHelp"
+                placeholder="Password"
+                ref={passwordRef}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="passwordConfirm">Confirm Password</label>
+              <input
+                type="password"
+                className="form-control form_input"
+                aria-describedby="passwordConfirmHelp"
+                placeholder="Confirm Password"
+                ref={passwordConfirmRef}
+                required
+              />
+            </div>
+            <button className="send_button" type="submit">
+              Submit
+            </button>
+          </form>
         </div>
-    )
+        <div className="login_body">Already have an account? Log In</div>
+      </div>
+    </div>
+  );
 }
